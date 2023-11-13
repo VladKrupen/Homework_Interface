4d2c8e947cc542e374c582e298027de35956f416
Vlad Krupen <air7360@gmail.com>
2023-11-13 22:07:47 +0300

    CustomView created

--- Homework_Interface/ViewController.swift
+++ Homework_Interface/ViewController.swift
@@ -7,13 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, ViewDelegate {
+    
+    let customView = {
+        let customView = View()
+        customView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return customView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        customView.delegate = self
+        view.addSubview(customView)
+        
+        NSLayoutConstraint.activate([
+            customView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            customView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
+        ])
+        
+        
     }
-
-
+    
+    func displayTexts(firstText: String, secondText: String, thirdText: String) {
+        print(firstText)
+        print(secondText)
+        print(thirdText)
+    }
+    
+    
+    
 }
-
