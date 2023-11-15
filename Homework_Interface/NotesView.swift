88db307871e02b8826f265cbeadd7433766acd18
Vlad Krupen <air7360@gmail.com>
2023-11-15 21:37:04 +0300

    NotesView and ButtonView added

---
+++ Homework_Interface/NotesView.swift
@@ -0,0 +1,60 @@
+//
+//  NotesView.swift
+//  Homework_Interface
+//
+//  Created by Vlad on 15.11.23.
+//
+
+import UIKit
+
+protocol NotesViewDelegate: AnyObject {
+    func displayTextView(text: String)
+}
+
+class NotesView: UIView, UITextViewDelegate {
+    
+    weak var delegate: NotesViewDelegate?
+    
+    var notes = {
+        let notes = UITextView()
+        notes.translatesAutoresizingMaskIntoConstraints = false
+        notes.backgroundColor = .gray
+        notes.text = "Notes: "
+        notes.font = UIFont.systemFont(ofSize: 20)
+        
+        return notes
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(notes)
+        notes.delegate = self
+        
+        NSLayoutConstraint.activate([
+            notes.topAnchor.constraint(equalTo: topAnchor),
+            notes.bottomAnchor.constraint(equalTo: bottomAnchor),
+            notes.trailingAnchor.constraint(equalTo: trailingAnchor),
+            notes.leadingAnchor.constraint(equalTo: leadingAnchor)
+        ])
+       
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+//    func textViewDidBeginEditing(_ textView: UITextView) {
+//        delegate?.displayTextView(text: notes.text)
+//    }
+//    
+//    func textViewDidEndEditing(_ textView: UITextView) {
+//        delegate?.displayTextView(text: notes.text)
+//    }
+    
+    func textViewDidChangeSelection(_ textView: UITextView) {
+        delegate?.displayTextView(text: notes.text)
+    }
+}
--- Homework_Interface/ViewController.swift
+++ Homework_Interface/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, ViewDelegate {
+class ViewController: UIViewController, ViewDelegate, ButtonViewDelegate, NotesViewDelegate {
     
     let customView = {
         let customView = View()
@@ -16,15 +16,44 @@ class ViewController: UIViewController, ViewDelegate {
         return customView
     }()
     
+    let notesView = {
+        let notesView = NotesView()
+        notesView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return notesView
+    }()
+    
+    let buttonView = {
+        let buttonView = ButtonView()
+        buttonView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return buttonView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.backgroundColor = .white
         customView.delegate = self
+        buttonView.delegate = self
+        notesView.delegate = self
         view.addSubview(customView)
+        view.addSubview(notesView)
+        view.addSubview(buttonView)
         
         NSLayoutConstraint.activate([
             customView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
-            customView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
+            customView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            
+            buttonView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            buttonView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            buttonView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            buttonView.heightAnchor.constraint(equalToConstant: 70),
+            
+            notesView.topAnchor.constraint(equalTo: customView.bottomAnchor),
+            notesView.bottomAnchor.constraint(equalTo: buttonView.topAnchor),
+            notesView.trailingAnchor.constraint(equalTo: customView.trailingAnchor),
+            notesView.leadingAnchor.constraint(equalTo: customView.leadingAnchor)
         ])
         
         
@@ -36,6 +65,26 @@ class ViewController: UIViewController, ViewDelegate {
         print(thirdText)
     }
     
+    func buttonSavePressed(_ sender: ButtonView) {
+        print(customView.firstTextField.text ?? "")
+        print(customView.middleTextField.text ?? "")
+        print(customView.lastTextField.text ?? "")
+    }
+    
+    func buttonCancelPressed(_ sender: ButtonView) {
+        customView.firstTextField.text = ""
+        customView.middleTextField.text = ""
+        customView.lastTextField.text = ""
+        notesView.notes.text = "Notes: "
+    }
+   
+    func buttonClearPressed(_ sender: ButtonView) {
+        notesView.notes.text = "Notes: "
+    }
+    
+    func displayTextView(text: String) {
+        print(text)
+    }
     
     
 }
